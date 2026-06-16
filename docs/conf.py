3b062bb2262a61af3d4364c68ab9129d0d3246a5
Sushma Gurram <sgurram@openteams.com>
2026-06-16 19:55:30 +0000

    Added the Landing page using Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "databridge"
+copyright = "2026, OpenTeams"  # noqa: A001
+author = "Databridge Team"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# MyST-Parser: enable colon-fence syntax and auto-generate heading anchors
+myst_enable_extensions = ["colon_fence"]
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "show_toc_level": 2,
+}
